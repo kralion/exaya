c4cde249d6840e2156e7404561bbbe5965c93714
Joan <joan300501@gmail.com>
2023-08-29 13:27:45 -0700

    Copyright Alignment Resolved #11

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -33,8 +33,8 @@ const MyApp: AppType<{ session: Session | null }> = ({
           <Component {...pageProps} />
         </ConfigProvider>
       </QueryClientProvider>
-      <footer className="my-4 text-center text-sm text-slate-500 drop-shadow-sm">
-        © Copyright 2024 Brayan Paucar. All rights reserved.
+      <footer className="mt-7 text-center text-sm text-slate-500 drop-shadow-sm">
+        © Copyright 2024 Brayan Paucar . All rights reserved.
       </footer>
     </SessionProvider>
   );
