10629cc8bff0fa082941a9c1032195692282d0c7
Joan <joan300501@gmail.com>
2023-10-08 16:07:31 -0500

    Resolved #54 , Roles colors identifiers, default roles, names

--- src/components/user-info.tsx
+++ src/components/user-info.tsx
@@ -10,32 +10,35 @@ export default function UserInfo({ collapsed }: UserInfoProps) {
 
   return (
     <Space wrap size={1} className="my-16 flex flex-col">
-      {!collapsed ? (
-        <Avatar
-          className="box-border border-1 border-gray-400"
-          size={60}
-          src="https://randomuser.me/api/portraits/men/95.jpg"
-        />
-      ) : (
-        <Avatar
-          className="box-border border-1 border-gray-400"
-          size={35}
-          src="https://randomuser.me/api/portraits/men/95.jpg"
-        />
-      )}
+      <Avatar
+        className=""
+        size={collapsed ? 35 : 80}
+        //TODO: Descomment this line when you have the user image
+        // src={
+        //   session?.user?.image ||
+        //   "https://cdn-icons-png.flaticon.com/128/8509/8509694.png?ga=GA1.1.631442079.1696688262"
+        // }
+        src="https://randomuser.me/api/portraits/men/95.jpg"
+      />
 
       {!collapsed && (
         <div className="flex flex-col items-center justify-center">
           <Typography.Text strong>
-            {session?.user?.name || "Gerardo Aguirre"}
+            {/*//TODO: Descomment this line when you have the user name            
+            {session?.user?.name || "Usuario Default"} */}
+            Santiago Vargas
           </Typography.Text>
           <Tag
             color={
-              session?.user?.role === "admin" ? "blue-inverse" : "gold-inverse"
+              session?.user?.role === "admin"
+                ? "gold-inverse"
+                : session?.user?.role === "supervisor"
+                ? "blue-inverse"
+                : "green-inverse"
             }
-            className="mt-1 rounded-full font-semibold"
+            className="mt-1 rounded-full font-semibold lowercase"
           >
-            admin
+            {session?.user?.role || "usuario"}
           </Tag>
         </div>
       )}
