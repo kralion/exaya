cfa25259521ec0f2cd179c80143ceb05579bdfe3
Brayan <joan300501@gmail.com>
2024-01-14 08:43:45 -0500

    Refactor needle function to use TypeScript and add type annotations

--- src/components/ui/administracion/kpi-chart.tsx
+++ src/components/ui/administracion/kpi-chart.tsx
@@ -15,7 +15,17 @@ const iR = 50;
 const oR = 100;
 const value = 82.07;
 
-const needle = (value, data, cx, cy, iR, oR, color) => {
+type needleProps = {
+  value: number;
+  data: any;
+  cx: number;
+  cy: number;
+  iR: number;
+  oR: number;
+  color: string;
+};
+
+const needle = ({ value, data, cx, cy, iR, oR, color }: needleProps) => {
   let total = 0;
   data?.forEach((v) => {
     total += v.value;
