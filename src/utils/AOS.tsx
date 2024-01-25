0c728b4822bd01096cfeb4c040f80e58c9807a46
Joan <joan300501@gmail.com>
2024-01-25 13:47:02 -0500

    Refactoring for deployment

--- src/utils/AOS.tsx
+++ src/utils/AOS.tsx
@@ -1,21 +1,20 @@
-import React, { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import React from "react";
 
 type ChildrenP = {
   children: React.ReactNode;
 };
 
 function AOSWrapper({ children }: ChildrenP) {
-  useEffect(() => {
+  React.useEffect(() => {
     AOS.init({
       duration: 2000,
     });
 
-    window.addEventListener("scroll", AOS.refresh());
-
+    // window.addEventListener("scroll", AOS.refresh())
     return () => {
-      window.removeEventListener("scroll", AOS.refresh());
+      // window.removeEventListener("scroll", AOS.refresh());
     };
   }, []);
 
