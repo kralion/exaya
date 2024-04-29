2a2400ac376407d56f9bcda2ff476d97f69358e2
Brayan <joan300501@gmail.com>
2024-04-29 11:25:26 -0500

    Add support for NETLIFY_URL environment variable in api.ts

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -10,13 +10,12 @@ import { type inferRouterInputs, type inferRouterOutputs } from "@trpc/server";
 import superjson from "superjson";
 
 import { type AppRouter } from "@/server/api/root";
-
 const getBaseUrl = () => {
   if (typeof window !== "undefined") return ""; // browser should use relative url
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
+  if (process.env.NETLIFY_URL) return `https://${process.env.NETLIFY_URL}`; // SSR should use netlify url
   return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
 };
-
 /** A set of type-safe react-query hooks for your tRPC API. */
 export const api = createTRPCNext<AppRouter>({
   config() {
