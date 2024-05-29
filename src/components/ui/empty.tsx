50faa6a04f6b57aceaa11cd970972fdcb1648e0d
Brayan <joan300501@gmail.com>
2024-05-29 07:33:33 -0500

    Refactor EmptyCustomized component: update image and description

--- src/components/ui/empty.tsx
+++ src/components/ui/empty.tsx
@@ -1,17 +1,15 @@
-import { Empty, Space, Typography } from "antd";
-import React from "react";
+import { Empty, Typography } from "antd";
 
 export default function EmptyCustomized() {
   return (
-    <Space direction="vertical" className="items-center justify-center">
-      <Empty
-        className="drop-shadow-xl"
-        image="https://img.icons8.com/?size=80&id=rAcqajy3zM5W&format=png"
-        imageStyle={{ height: 60 }}
-      />
-      <Typography.Text type="secondary">
-        Se deben registrar datos para visualizar información.
-      </Typography.Text>
-    </Space>
+    <Empty
+      image="https://img.icons8.com/?size=48&id=pMqR06nxZSFn&format=png"
+      imageStyle={{ height: 50 }}
+      description={
+        <Typography.Text type="secondary">
+          No hay datos para mostrar
+        </Typography.Text>
+      }
+    />
   );
 }
