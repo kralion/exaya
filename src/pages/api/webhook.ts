9f2a95f219f4862dd5ecd8630ea1429a168003ce
Brayan Paucar <joan300501@gmail.com>
2024-06-10 16:59:29 -0500

    feat: Verify webhook signature and handle payment events

--- src/pages/api/webhook.ts
+++ src/pages/api/webhook.ts
@@ -1,10 +1,11 @@
 import crypto from "crypto";
-
+import { NextResponse } from "next/server";
 type PaymentEvent = {
   meta: {
     custom_data: {
       user_id: string;
     };
+    event_name: "order_created" | "order_refunded";
   };
   data: {
     attributes: {
@@ -13,36 +14,35 @@ type PaymentEvent = {
   };
 };
 
-export async function POST(req: Request) {
-  try {
-    const eventType = req.headers.get("X-Event-Name");
-    const body: PaymentEvent = (await req.json()) as PaymentEvent;
+export default async function POST(request: Request) {
+  const body = (await request.json()) as PaymentEvent;
+  const sigString = request.headers.get("x-signature");
+  const secret = process.env.LEMON_SQUEEZY_WEBHOOK_SIGNATURE;
+  const hmac = crypto.createHmac("sha256", secret as string);
+  const digest = Buffer.from(
+    hmac.update(JSON.stringify(body)).digest("hex"),
+    "utf8"
+  );
 
-    // Verify signature
-    const secret = process.env.LEMON_SQUEEZY_WEBHOOK_SIGNATURE;
-    const hmac = crypto.createHmac("sha256", secret as string);
-    const digest = Buffer.from(
-      hmac.update(await req.clone().text()).digest("hex"),
-      "utf8"
-    );
-    const signature = Buffer.from(req.headers.get("X-Signature") || "", "utf8");
-
-    if (!crypto.timingSafeEqual(digest, signature)) {
-      return Response.json({ message: "Invalid signature" }, { status: 400 });
-    }
-
-    if (eventType === "order_paid" && body.data.attributes.status === "paid") {
-      // Payment is successful
-      console.log("Payment successful for order");
-
-      return Response.json({ message: "Payment successful" }, { status: 200 });
-    }
-
-    // For other events or failed payments
-    console.log("Event received or payment not successful");
-    return Response.json({ message: "Event received" }, { status: 200 });
-  } catch (err) {
-    console.error(err);
-    return Response.json({ message: "Server error" }, { status: 500 });
+  const signature = Buffer.from(
+    Array.isArray(sigString) ? sigString.join("") : sigString || "",
+    "utf8"
+  );
+  if (!crypto.timingSafeEqual(digest, signature)) {
+    throw new Error("Invalid signature");
   }
+  console.log(body);
+
+  const eventName = body.meta.event_name;
+
+  if (eventName === "order_created") {
+    const user_id = body.meta.custom_data.user_id;
+    const isSucessful = body.data.attributes.status === "paid";
+    if (isSucessful) {
+      console.log("order created", user_id);
+    } else {
+      console.log("order failed", user_id);
+    }
+  }
+  return NextResponse.json({ result: true }, { status: 200 });
 }
